f636508114bf5cf4d24d7dad9d9b7d3b8ba1092a
Callum Mackintosh <callum.e.s.mackintosh@gmail.com>
2023-04-27 20:35:12 +1000

    Updated Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
-    return (
-      <header className="header">
-        <div className="logo">
-          <h1>My Portfolio</h1>
-        </div>
-      </header>
-    );
-  }
-  
+  return (
+    <header className="header">
+      <div className="logo">
+        <h1>My Portfolio</h1>
+      </div>
+      <nav className="navigation">
+        <ul>
+          <li>
+            <Link to="/about">About Me</Link>
+          </li>
+          <li>
+            <Link to="/portfolio">Portfolio</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/resume">Resume</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
 export default Header;
