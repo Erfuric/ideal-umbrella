cd2fbe4e8ab39655d80ac141b0820ce09584cb97
Callum Mackintosh <callum.e.s.mackintosh@gmail.com>
2023-04-27 20:02:10 +1000

    updated index.js and app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import AboutMe from './containers/AboutMe';
+import Portfolio from './containers/Portfolio';
+import Contact from './containers/Contact';
+import Resume from './containers/Resume';
 
 function App() {
+  const [selected, setSelected] = useState('About Me');
+
+  const renderContent = () => {
+    switch (selected) {
+      case 'About Me':
+        return <AboutMe />;
+      case 'Portfolio':
+        return <Portfolio />;
+      case 'Contact':
+        return <Contact />;
+      case 'Resume':
+        return <Resume />;
+      default:
+        return <AboutMe />;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Navigation selected={selected} setSelected={setSelected} />
+      {renderContent()}
+    </>
   );
 }
 
