ad09993ffa896e9b345240ab6abdc2629f7a058d
Callum Mackintosh <callum.e.s.mackintosh@gmail.com>
2023-04-27 21:05:09 +1000

    Commented out link

--- src/containers/Resume/index.js
+++ src/containers/Resume/index.js
@@ -4,7 +4,7 @@ function Resume() {
   return (
     <div className="resume">
       <h2>Resume</h2>
-      <p>Click <a href="#">here</a> to download my resume in PDF format.</p>
+      {/* <p>Click <a href="#">here</a> to download my resume in PDF format.</p> */}
     </div>
   );
 }
