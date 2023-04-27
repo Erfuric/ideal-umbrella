8f6b3914aa7798b01c308efd8bcaf62680534ed7
Callum Mackintosh <callum.e.s.mackintosh@gmail.com>
2023-04-27 20:44:28 +1000

    Resume with placeholder for pdf to add later

--- src/containers/Resume/index.js
+++ src/containers/Resume/index.js
@@ -1,1 +1,12 @@
-// Creating folder structure
+import React from 'react';
+
+function Resume() {
+  return (
+    <div className="resume">
+      <h2>Resume</h2>
+      <p>Click <a href="#">here</a> to download my resume in PDF format.</p>
+    </div>
+  );
+}
+
+export default Resume;
